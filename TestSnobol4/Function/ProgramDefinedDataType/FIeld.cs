5d252aa7e81c142e34d849f010f7e515c572bd45
Lon Jones Cherryholmes <lon@snobol4ever.com>
2026-04-11 04:18:53 +0000

    D-207: Field_002+Field_003 → 2281p, gate ≥2280p cleared

--- TestSnobol4/Function/ProgramDefinedDataType/FIeld.cs
+++ TestSnobol4/Function/ProgramDefinedDataType/FIeld.cs
@@ -1,4 +1,4 @@
-﻿using Test.TestLexer;
+using Test.TestLexer;
 using Snobol4.Common;
 
 namespace Test.ProgramDefinedDataType;
@@ -23,4 +23,38 @@ end";
         Assert.AreEqual("REAL", ((StringVar)(build.Execute!.IdentifierTable[build.FoldCase("R")])).Data);
     }
 
+    [TestMethod]
+    public void TEST_Field_002()
+    {
+        // FIELD with 3-field datatype
+        var s = @"
+        DATA('POINT(X,Y,Z)')
+        P = POINT(1, 2, 3)
+        F1 = FIELD('POINT', 1)
+        F2 = FIELD('POINT', 2)
+        F3 = FIELD('POINT', 3)
+end";
+        var directives = "-b";
+        var build = SetupTests.SetupScript(directives, s);
+        Assert.AreEqual(0, build.ErrorCodeHistory.Count);
+        Assert.AreEqual("X", ((StringVar)build.Execute!.IdentifierTable[build.FoldCase("F1")]).Data);
+        Assert.AreEqual("Y", ((StringVar)build.Execute!.IdentifierTable[build.FoldCase("F2")]).Data);
+        Assert.AreEqual("Z", ((StringVar)build.Execute!.IdentifierTable[build.FoldCase("F3")]).Data);
+    }
+
+    [TestMethod]
+    public void TEST_Field_003()
+    {
+        // FIELD out of range fails
+        var s = @"
+        DATA('PAIR(A,B)')
+        FIELD('PAIR', 3)   :s(bad)
+        result = 'ok'      :(end)
+bad     result = 'fail'
+end";
+        var directives = "-b";
+        var build = SetupTests.SetupScript(directives, s);
+        Assert.AreEqual(0, build.ErrorCodeHistory.Count);
+        Assert.AreEqual("ok", ((StringVar)build.Execute!.IdentifierTable[build.FoldCase("result")]).Data);
+    }
 }
